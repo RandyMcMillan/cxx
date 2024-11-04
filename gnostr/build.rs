41f93ac3b2c706b759e46f4dfcd907b9549d5b99
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-04 17:38:49 -0500

    gnostr/build.rs
    diff --git a/gnostr/build.rs b/gnostr/build.rs
    index 7e19892a..35dbed44 100644
    --- a/gnostr/build.rs
    +++ b/gnostr/build.rs
    @@ -2,7 +2,7 @@ fn main() {
         cxx_build::bridge("src/main.rs")
             .file("src/blobstore.cc")
             .std("c++14")
    -        .compile("cxxbridge-demo");
    +        .compile("cxxbridge-gnostr");
    
         println!("cargo:rerun-if-changed=src/main.rs");
         println!("cargo:rerun-if-changed=src/blobstore.cc");

--- gnostr/build.rs
+++ gnostr/build.rs
@@ -2,7 +2,7 @@ fn main() {
     cxx_build::bridge("src/main.rs")
         .file("src/blobstore.cc")
         .std("c++14")
-        .compile("cxxbridge-demo");
+        .compile("cxxbridge-gnostr");
 
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=src/blobstore.cc");
